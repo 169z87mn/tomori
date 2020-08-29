68683a0040bd5c3e87f51cf0217b63ad7dea2434
169z87mn <169z87mn@gmail.com>
2020-08-30 05:56:38 +0900

    これはナビバー

---
+++ src/components/header/nav-bar.vue
@@ -0,0 +1,17 @@
+<template>
+  <b-navbar toggleable type="dark" variant="dark">
+    <b-navbar-brand href="#">tomori</b-navbar-brand>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: 'nav-bar',
+  components: {
+    
+  }
+}
+</script>
+
+<style lang="">
+</style>
