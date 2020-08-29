80ba08bd7fb5f355e43683803285edc88cf04857
169z87mn <169z87mn@gmail.com>
2020-08-30 05:57:40 +0900

    radioのタブっぽいの

---
+++ src/components/radio/radio-tab.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <b-tabs pills card>
+      <div v-for="(tab, i) in tabs" :key="i">
+        <b-tab :title="tab">
+          <RadioList :tab="i"/>
+        </b-tab>
+      </div>
+    </b-tabs>
+  </div>
+</template>
+
+<script>
+import RadioList from './radio-list'
+
+export default {
+  components: {
+    RadioList
+  },
+  data: () => ({
+    tabs: ["月", "火", "水", "木", "金", "土", "日"]
+  })
+}
+</script>
